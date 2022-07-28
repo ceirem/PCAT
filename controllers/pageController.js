8418f1323e3b9901494791dc2dcbac7a352a9e69
ceirem <72570708+ceirem@users.noreply.github.com>
2022-07-28 16:42:19 +0300

    MVC is done

---
+++ controllers/pageController.js
@@ -0,0 +1,21 @@
+const Photo = require ('../models/Photo')
+
+
+exports.getAboutPage = (req, res) => {
+    res.render('about');
+  }
+
+exports.getAddPostPage = (req, res) => {
+    res.render('add_post');
+  }
+
+exports.getPostPage =  (req, res) => {
+    res.render('post');
+  }
+
+exports.getEditPage = async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id });
+    res.render('edit', {
+      photo,
+    });
+  }
